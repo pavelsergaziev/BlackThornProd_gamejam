30e5f20c3f450fe27772e8b35819b76ea2fce3eb
PavelSergaziev <dargoth@narod.ru>
2018-10-15 14:08:11 +0300

    продолжаю с генератором
    осталось генератор задников-передников и генератор всякого висящего в воздухе. И сделать проверку, чтобы бафф не лежал прямо на дебаффе и т.п.

---
+++ BlackThornProd gamejam project/Assets/Scripts/Level Generation/TimedLevelElementsGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TimedLevelElementsGenerator : BaseLevelElementsGenerator
+{
+
+    private int _levelGenerationLoopCyclesToNextObjectPlacement;
+    private int _minDelayBetweenObjectPlacements;
+    private int _maxDelayBetweenObjectPlacements;
+
+    public TimedLevelElementsGenerator(int objectsPoolSize, Transform levelLayoutObject, GameObject objectPrefab, int delayBeforeFirstPlacement, int minDelayBetweenObjectPlacements, int maxDelayBetweenObjectPlacements) : base(objectsPoolSize, levelLayoutObject, objectPrefab)
+    {
+        _minDelayBetweenObjectPlacements = minDelayBetweenObjectPlacements;
+        _maxDelayBetweenObjectPlacements = maxDelayBetweenObjectPlacements;
+        _levelGenerationLoopCyclesToNextObjectPlacement = delayBeforeFirstPlacement;
+    }
+
+    protected bool CheckIsItTimeToPlaceObject()
+    {
+        if (_levelGenerationLoopCyclesToNextObjectPlacement > 0)
+        {
+            _levelGenerationLoopCyclesToNextObjectPlacement--;
+            return false;
+        }
+        else
+        {
+            _levelGenerationLoopCyclesToNextObjectPlacement = Random.Range(_minDelayBetweenObjectPlacements, _maxDelayBetweenObjectPlacements + 1);
+            return true;
+        }
+    }
+}
